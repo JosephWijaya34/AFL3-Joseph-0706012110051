6a86f59328180e35dc4496ec54e66351f4c095e5
JosephWijaya34 <josephwijaya01@gmail.com>
2023-04-18 09:13:37 +0700

    Initial Commit

---
+++ AFL3-Joseph-0706012110051/AFL3_Joseph_0706012110051App.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_Joseph_0706012110051App.swift
+//  AFL3-Joseph-0706012110051
+//
+//  Created by MacBook Pro on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_Joseph_0706012110051App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFL3-Joseph-0706012110051/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AFL3-Joseph-0706012110051
+//
+//  Created by MacBook Pro on 18/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
