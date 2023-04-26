2e69d6fdbd240f11804b8c9677226d945b6d198b
JosephWijaya34 <josephwijaya01@gmail.com>
2023-04-26 15:36:19 +0700

    chapter 3 part 2

---
+++ AFL3-Joseph-0706012110051/Profiles/ProfileHost.swift
@@ -0,0 +1,50 @@
+//
+//  ProfileHost.swift
+//  AFL3-Joseph-0706012110051
+//
+//  Created by MacBook Pro on 26/04/23.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            // cancel edit button
+            HStack {
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel", role: .cancel) {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
+                
+                Spacer()
+                EditButton()
+            }
+            // edit button
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        modelData.profile = draftProfile
+                    }
+            }
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost().environmentObject(ModelData())
+    }
+}
